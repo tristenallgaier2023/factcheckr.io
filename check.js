b929d2667b0be4b608c56cddcdd1c0411db963b7
Tristen Allgaier <tristenallgaier2023@u.northwestern.edu>
2022-10-24 16:48:36 -0500

    integrate google fact check

--- check.js
+++ check.js
@@ -1,10 +1,17 @@
 async function check() {
     let input_claim = document.getElementById("input_link").value;
+    console.log(`Checking \"${input_claim}\"`);
 
+    // fact check with claim buster
+    check_claim_buster(input_claim);
+
+    // fact check with google
+    check_google(input_claim);
+}
+
+async function check_claim_buster(input_claim) {
     // The key should be placed somewhere else later
     let api_key = '156503534eed411ca6d4182549051a4a';
-
-    console.log(`Checking \"${input_claim}\"`);
 
     // Setup the Fetch GET Request with the appropriate headers and URL
     let response = await fetch(`https://idir.uta.edu/claimbuster/api/v2/query/knowledge_bases/${input_claim}`, {
@@ -16,5 +23,19 @@ async function check() {
     .then(response => response.json())
     .then(data => {
         console.log(data.justification);
+    });
+}
+
+async function check_google(input_claim) {
+    // The key should be placed somewhere else later
+    let api_key = 'AIzaSyCz7xKnP_rVlK8mhVMaA3wvYnd4L9ziHVM';
+
+    // Setup the Fetch GET Request with the appropriate headers and URL
+    let response = await fetch(`https://factchecktools.googleapis.com/v1alpha1/claims:search?query=${input_claim}&key=${api_key}`, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
     });
 }
